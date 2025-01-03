11014c770a28361324becfe11df53ad927f6f73f
Michael540coder <michaellyonelruiz@gmail.com>
2025-01-03 00:45:55 -0500

    Added new joker: Doubled Joker

--- H1DROmod/H1DROmod.lua
+++ H1DROmod/H1DROmod.lua
@@ -1,6 +1,7 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: H1DRO Mod
 --- MOD_ID: H1DROmod
+--- PREFIX: HMOD
 --- MOD_AUTHOR: [H1DRO]
 --- MOD_DESCRIPTION: Interesting, or extremely dumb stuff..
 
@@ -58,18 +59,72 @@ SMODS.Joker {
 
         if left_card ~= nil then
             left_card:start_dissolve()
-            local card_data = {}
-            local new_card = SMODS.create_card({
-                set = 'Joker',
-                area = G.jokers,
-                rarity = translatething(left_card.config.center.rarity),
-                legendary = doothertranslate(left_card.config.center.rarity)
-            })
-            G.jokers:emplace(new_card)
-            new_card:add_to_deck()
+            if G.jokers.config.card_limit ~= #G.jokers.cards then
+                SMODS.add_card({
+                    set = 'Joker',
+                    area = G.jokers,
+                    rarity = translatething(left_card.config.center.rarity),
+                    legendary = doothertranslate(left_card.config.center.rarity)
+                })
+            end
         end
     end
 }
 
+SMODS.Joker {
+    key = 'doubledJoker',
+    loc_txt = {
+        name = 'Doubled Joker',
+        text = {
+            "Creates an extra copy",
+            "of the joker to",
+            "left of this card",
+            "{C:inactive}(If there's room)",
+            "{C:attention}-1{} Joker slots"
+        }
+    },
+    rarity = 3,
+    atlas = 'atlas',
+    pos = {x = 1, y = 0},
+    cost = 4,
+    config = {extra = {slot_removed = false}},
+    calculate = function(self, card, context)
+        local left_card
+        if context.joker_main then
+            for i = #G.jokers.cards, 1, -1 do
+                if G.jokers.cards[i] == card then
+                    local left_index = i - 1
+                    if left_index > 0 and G.jokers.cards[left_index] then
+                       left_card = G.jokers.cards[left_index]
+                    end
+                end
+            end
+        end
+        if left_card ~= nil then
+            if G.jokers.config.card_limit ~= #G.jokers.cards or left_card.edition.negative then
+                if left_card.edition then
+                    add_joker(left_card.config.center_key, left_card.edition.type)
+                else
+                    add_joker(left_card.config.center_key)
+                end
+            end
+        end
+    end,
+
+    add_to_deck = function(self, card, from_debuff)
+        if not card.ability.extra.slot_removed then
+            if not card.ability.extra.slot_removed and G.jokers.config.card_limit > 1 then
+                G.jokers.config.card_limit = G.jokers.config.card_limit - 1
+                card.ability.extra.slot_removed = true
+            end
+        end
+    end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        if card.ability.extra.slot_removed then
+            G.jokers.config.card_limit = G.jokers.config.card_limit + 1
+        end
+    end
+}
 ----------------------------------------------
 ------------MOD CODE END----------------------
